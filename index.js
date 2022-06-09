c99fa2e7d387c197be881d42c51d2dab9c16d0d1
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-09 12:28:40 +0100

    commit tree a96db7b448324d70c2fc097d4472f735f5df61d4

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+var customerName ='BOB';
+
+function upperCaseCustomerName () {
+    return customerName;
+}
+
+function setBestCustomer (){
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
