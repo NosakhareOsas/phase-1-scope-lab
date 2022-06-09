b8c44aff247192b4774bb5fd0457f0989799a162
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-09 12:53:20 +0100

    Test Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName ='bob';
+var bestCustomer;
+const leastFavoriteCustomer = "sam";
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+function setBestCustomer (){
+    return bestCustomer = 'not bob';    
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'chris';
+}
